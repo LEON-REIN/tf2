0571bcc0cf75917cf695344d7fbc1bc2519dd077
LEON-REIN <r1345591989@163.com>
2020-05-22 01:16:48 +0800

    nothing

--- keras/cifar2.py
+++ keras/cifar2.py
@@ -10,17 +10,22 @@
 
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
+from matplotlib import pyplot as plt
+
+'''
+1. Load datasets-sets && 2. Data Processing
+'''
 
 BATCH_SIZE = 100
 
 
 def load_image(img_path, size=(32, 32)):
-    label = tf.constant(1, tf.int8) if tf.strings.regex_full_match(img_path, ".*automobile.*") \
+    labels = tf.constant(1, tf.int8) if tf.strings.regex_full_match(img_path, ".*automobile.*") \
         else tf.constant(0, tf.int8)  # automobile labeled as 1, while airplane represents 0
-    img = tf.io.read_file(img_path)
-    img = tf.image.decode_jpeg(img)  # jpeg format!
-    img = tf.image.resize(img, size) / 255.0  # Resize images to 'size' and normalization
-    return img, label
+    imgs = tf.io.read_file(img_path)
+    imgs = tf.image.decode_jpeg(imgs)  # jpeg format!
+    imgs = tf.image.resize(imgs, size) / 255.0  # Resize images to 'size' and normalization
+    return imgs, labels
 
 
 # 使用并行化预处理 num_parallel_calls 和预存数据 prefetch 来提升性能
@@ -34,5 +39,37 @@ ds_test = tf.data.Dataset.list_files(r"datasets\cifar2_datasets\test\*\*.jpg") \
     .batch(BATCH_SIZE) \
     .prefetch(tf.data.experimental.AUTOTUNE)
 
+# Show Some Pictures
+# plt.figure(figsize=(8, 8))
+# for i, (img, label) in enumerate(ds_train.unbatch().take(9)):
+#     ax = plt.subplot(3, 3, i + 1)
+#     ax.imshow(img.numpy())
+#     ax.set_title("label = %d" % label)
+#     ax.set_xticks([])
+#     ax.set_yticks([])
+# plt.show()
+
+# for x, y in ds_train.take(1):
+#     print(x.shape, y.shape)  # (100, 32, 32, 3) (100,)
+
+'''
+3. Keras Modeling (Using  Functional APIs)
+'''
+
+tf.keras.backend.clear_session()  # Clear sessions
+
+inputs = layers.Input(shape=(32, 32, 3))
+x = layers.Conv2D(32, kernel_size=(3, 3))(inputs)
+x = layers.MaxPool2D()(x)
+x = layers.Conv2D(64, kernel_size=(5, 5))(x)
+x = layers.MaxPool2D()(x)
+x = layers.Dropout(rate=0.1)(x)
+x = layers.Flatten()(x)
+x = layers.Dense(32, activation='relu')(x)
+outputs = layers.Dense(1, activation='sigmoid')(x)
+
+model = models.Model(inputs=inputs, outputs=outputs)
+
+# model.summary()
 
 
